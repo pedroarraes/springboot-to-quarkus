e6ca1d0d2f449355389e1a2f494477bdd4346745
Pedro Arraes <pedroarraes@gmail.com>
2023-06-30 17:57:49 -0300

    Finalizado

--- src/main/resources/data.sql
+++
@@ -1,5 +0,0 @@
-INSERT INTO products (name, description, price, created_at, updated_at) VALUES('Iphone X', 'Iphone X 64GB', 47499, NOW(), NOW());
-INSERT INTO products (name, description, price, created_at, updated_at) VALUES('JBL Clip 2', 'Portable Speaker', 1499, NOW(), NOW());
-INSERT INTO products (name, description, price, created_at, updated_at) VALUES('Huawei Nova 3i', '128GB - 4GB with Free Bluetooth Speaker', 15990, NOW(), NOW());
-INSERT INTO products (name, description, price, created_at, updated_at) VALUES('LG SMART TV FHD 43"', 'Model LG 43LJ5500 (2017)', 21999, NOW(), NOW());
-INSERT INTO products (name, description, price, created_at, updated_at) VALUES('DEVANT LED TV', '49" Basic LED TV', 15000, NOW(), NOW());
